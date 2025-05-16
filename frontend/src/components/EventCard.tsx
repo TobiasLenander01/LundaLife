5e019179ed4f72dd4d72a970ff3b738ed1b236c9
Victor Karlström <victor.karlstrom@outlook.com>
2025-05-16 13:55:50 +0200

    Add date logic

--- frontend/src/components/EventCard.tsx
+++ frontend/src/components/EventCard.tsx
@@ -3,6 +3,31 @@
 import { useEffect, useState } from "react";
 import { Event } from '@/types/db';
 
+function hej(event: Event) {
+    const start = new Date(event.start_date);
+    const end = event.end_date ? new Date(event.end_date) : null;
+
+    let the_end;
+
+    if (start.getDay !== end?.getDay) {
+        {
+            the_end = event.end_date ? new Date(event.end_date).toLocaleString('sv-SV', {
+                hour: '2-digit',
+                minute: '2-digit'
+            }) : ""
+        }
+    }
+    else {
+        the_end = event.end_date ? new Date(event.end_date).toLocaleString('sv-SV', {
+            day: 'numeric',
+            month: 'short',
+            hour: '2-digit',
+            minute: '2-digit'
+        }) : ""
+    }
+    return the_end
+}
+
 export default function EventCard() {
     const [events, setEvents] = useState<Event[]>([]);
 
@@ -28,14 +53,30 @@ export default function EventCard() {
 
     return (
         <div className="flex">
-            <h1>{event.name}</h1>
-            <div>
-                {new Date(event.start_date).toLocaleString()}
-                -
-                {event.end_date ? new Date(event.end_date).toLocaleString() : ""}
+            <div className="uppercase column">
+                <div>
+                    {new Date(event.start_date).toLocaleDateString('sv-SV', { month: 'short' })}
+                </div>
+                <div>
+                    {new Date(event.start_date).toLocaleDateString('sv-SV', { day: 'numeric' })}
+                </div>
             </div>
-            <div>{event.organization_name}</div>
-            <div>{event.address}</div>
+            <div className="column">
+                <div>
+                    {new Date(event.start_date).toLocaleString('sv-SV', {
+                        day: 'numeric',
+                        month: 'short',
+                        hour: '2-digit',
+                        minute: '2-digit'
+                    })}
+                    {" "} - {" "}
+                    {hej(event)}
+                </div>
+                <div>{event.name}</div>
+
+                <div>{event.organization_name}</div>
+            </div>
+
             <a href={event.link ?? ""}>See Event</a>
         </div>
     )
